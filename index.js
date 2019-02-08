dbf8ac93abb0d7642b1de312593fdd0b943dd60b
Riccardo <riki-95@hotmail.it>
2019-02-08 15:38:22 +0100

    ABI updated and multiTransfer skeleton

--- index.js
+++ index.js
@@ -60,39 +60,39 @@ app.get('/sendtx',function(req,res){
 });
 
 app.get('/sendmoretx',function(req,res){
+    var addresses = []
     for(var i = 0; i < 5;i++) {
-        console.log('Transaction number ' + i)
-        let newAccount = web3js.eth.accounts.create('try');
+        let newAccount = web3js.eth.accounts.create("try");
         var toAddress = newAccount.address;
         console.log('ToAddress: ' + 'https://ropsten.etherscan.io/address/' + toAddress)
-
-        var count;
-        // get transaction count, later will used as nonce
-        web3js.eth.getTransactionCount(myAddress).then(function(v){
-            count = v;
-            var amount = web3js.utils.toHex(100);
-
-            //creating raw tranaction
-            var rawTransaction = { 
-                "from": myAddress, 
-                "gasPrice": web3js.utils.toHex(20 * 1e9), 
-                "gasLimit": web3js.utils.toHex(210000), 
-                "to": contractAddress,
-                "data": contract.methods.mintToken(toAddress, amount).encodeABI(), 
-                "nonce": web3js.utils.toHex(count) 
-            }
-            var transaction = new Tx(rawTransaction);
-
-            //signing transaction with private key
-            transaction.sign(privateKey);
-            //sending transacton via web3js module
-            web3js.eth.sendSignedTransaction('0x'+transaction.serialize().toString('hex'))
-            .on('transactionHash', (transaction) => {
-                console.log('Transaction: ' + 'https://ropsten.etherscan.io/tx/' + transaction)
-                res.json({transactionHash: transaction})
-            });
-        })
+        addresses.push(toAddress.toString());
     }
+    var count;
+    // get transaction count, later will used as nonce
+    web3js.eth.getTransactionCount(myAddress).then(function(v){
+        count = v;
+        var amount = web3js.utils.toHex(1);
+
+        //creating raw tranaction
+        var rawTransaction = { 
+            "from": myAddress, 
+            "gasPrice": web3js.utils.toHex(20 * 1e9), 
+            "gasLimit": web3js.utils.toHex(210000), 
+            "to": contractAddress,
+            "data": contract.methods.drop(addresses, amount).encodeABI(), 
+            "nonce": web3js.utils.toHex(count) 
+        }
+        var transaction = new Tx(rawTransaction);
+
+        //signing transaction with private key
+        transaction.sign(privateKey);
+        //sending transacton via web3js module
+        web3js.eth.sendSignedTransaction('0x'+transaction.serialize().toString('hex'))
+        .on('transactionHash', (transaction) => {
+            console.log('Transaction: ' + 'https://ropsten.etherscan.io/tx/' + transaction)
+            res.json({transactionHash: transaction})
+        });
+    })
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
